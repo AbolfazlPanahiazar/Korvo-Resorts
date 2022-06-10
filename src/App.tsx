27bcf6892b4f3153c15f8bc47ebe7286e3ddd483
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2022-06-10 21:59:49 +0430

    Add application router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import { FC } from "react";
+import { Routes, Route } from "react-router-dom";
 
 import Provider from "components/Provider/Provider";
 
 const App: FC = () => {
-  return <Provider>hello</Provider>;
+  return (
+    <Provider>
+      <Routes>
+        <Route path="/" element={<h1>resorts</h1>} />
+        <Route path="/resorts/:id" element={<h1>single resport</h1>} />
+        <Route path="/bucket" element={<h1>bucket</h1>} />
+        <Route path="*" element={<h1>404</h1>} />
+      </Routes>
+    </Provider>
+  );
 };
 
 export default App;
